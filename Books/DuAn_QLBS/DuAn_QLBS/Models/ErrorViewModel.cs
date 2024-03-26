e8038ca5e4c75c62cc5039bb604889f92be8cc13
nhutlee0504 <149445955+nhutlee0504@users.noreply.github.com>
2024-03-26 16:42:02 +0700

    Add files via upload

---
+++ Books/DuAn_QLBS/DuAn_QLBS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DuAn_QLBS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
